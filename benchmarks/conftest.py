4dc1b6049cd81ad87f7299ea20126ffaf04a2241
Roman Snegirev <rsng@mail.ru>
2024-12-02 13:44:30 +0300

    Add benchmarks

---
+++ benchmarks/conftest.py
@@ -0,0 +1,120 @@
+# pylint:disable=redefined-outer-name
+
+import asyncio
+import pytest_asyncio
+from motor.motor_asyncio import AsyncIOMotorClient
+from motor.core import AgnosticDatabase as MotorDatabase
+from pymongo import AsyncMongoClient
+from pymongo.asynchronous.database import AsyncDatabase
+
+
+from mongojet import create_client, Database as MongojetDatabase
+
+DB_URL = 'mongodb://127.0.0.1:27017/benchmarks?maxPoolSize=16'
+READ_COLLECTION_NAME = 'collection_to_read'
+UPDATE_COLLECTION_NAME = 'collection_to_update'
+
+DOC_NUM = 128
+
+
+@pytest_asyncio.fixture(scope='session', autouse=True)
+async def init_db():
+    client = await create_client(DB_URL)
+    db = client.get_default_database()
+    collection = db[READ_COLLECTION_NAME]
+    await collection.delete_many({})
+    for i in range(DOC_NUM):
+        await collection.insert_one({'a': i})
+
+    yield
+
+    await db.drop()
+    await client.close()
+
+
+@pytest_asyncio.fixture(scope='session')
+async def mongojet_client():
+    c = await create_client(DB_URL)
+    yield c
+    await c.close()
+
+
+@pytest_asyncio.fixture(scope='session')
+async def motor_client():
+    c = AsyncIOMotorClient(DB_URL)
+    yield c
+    c.close()
+
+
+@pytest_asyncio.fixture(scope='session')
+async def pymongo_client():
+    c = AsyncMongoClient(DB_URL)
+    yield c
+    await c.close()
+
+
+@pytest_asyncio.fixture(scope='session')
+async def mongojet_db(mongojet_client):
+    return mongojet_client.get_default_database()
+
+
+@pytest_asyncio.fixture(scope='session')
+async def motor_db(motor_client):
+    return motor_client.get_default_database()
+
+
+@pytest_asyncio.fixture(scope='session')
+async def pymongo_db(pymongo_client: AsyncMongoClient):
+    return pymongo_client.get_default_database()
+
+
+@pytest_asyncio.fixture(scope='session')
+async def mongojet_read_collection(mongojet_db: MongojetDatabase):
+    return mongojet_db[READ_COLLECTION_NAME]
+
+
+@pytest_asyncio.fixture(scope='session')
+async def motor_read_collection(motor_db: MotorDatabase):
+    return motor_db[READ_COLLECTION_NAME]
+
+
+@pytest_asyncio.fixture(scope='session')
+async def pymongo_read_collection(pymongo_db: AsyncDatabase):
+    return pymongo_db[READ_COLLECTION_NAME]
+
+
+@pytest_asyncio.fixture(scope='function')
+async def mongojet_update_collection(mongojet_db: MongojetDatabase):
+    c = mongojet_db[UPDATE_COLLECTION_NAME]
+    await c.delete_many({})
+    return c
+
+
+@pytest_asyncio.fixture(scope='function')
+async def motor_update_collection(motor_db: MotorDatabase):
+    c = motor_db[UPDATE_COLLECTION_NAME]
+    await c.delete_many({})
+    return c
+
+
+@pytest_asyncio.fixture(scope='function')
+async def pymongo_update_collection(pymongo_db: AsyncDatabase):
+    c = pymongo_db[UPDATE_COLLECTION_NAME]
+    await c.delete_many({})
+    return c
+
+
+# https://github.com/ionelmc/pytest-benchmark/issues/66
+@pytest_asyncio.fixture
+def aio_benchmark(benchmark):
+    def _wrapper(func, *args, **kwargs):
+        if asyncio.iscoroutinefunction(func):
+
+            def run_coro(*pos, **kwd):
+                return asyncio.get_event_loop().run_until_complete(func(*pos, **kwd))
+
+            return benchmark(run_coro, *args, **kwargs)
+        else:
+            return benchmark(func, *args, **kwargs)
+
+    return _wrapper
---
+++ benchmarks/test_bench.py
@@ -0,0 +1,124 @@
+import asyncio
+
+import pytest
+from motor.core import AgnosticCollection as MotorCollection
+from pymongo.asynchronous.collection import AsyncCollection
+
+from mongojet import Collection as MongojetCollection
+
+ITERATIONS = 1
+CONCURENCIES = [
+    # 32,
+    # 64,
+    # 128,
+    # 256,
+    512,
+    # 4096,
+]
+
+
+async def find(
+    collection: MongojetCollection | MotorCollection | AsyncCollection,
+    iterations: int,
+    concurrency: int,
+):
+    async def fn():
+        for i in range(iterations):
+            await collection.find_one({'a': i})
+
+    tasks = [fn() for _ in range(concurrency)]
+    await asyncio.gather(*tasks)
+
+
+async def insert(
+    collection: MongojetCollection | MotorCollection | AsyncCollection,
+    iterations: int,
+    concurrency: int,
+):
+    async def fn():
+        for i in range(iterations):
+            await collection.insert_one({'a': i})
+
+    tasks = [fn() for _ in range(concurrency)]
+    await asyncio.gather(*tasks)
+
+
+async def cursor_mongojet(
+    collection: MongojetCollection,
+    iterations: int,
+    concurrency: int,
+):
+    async def fn():
+        for _ in range(iterations):
+            cur = await collection.aggregate(pipeline=[{'$match': {'a': {'$gt': 10}}}])
+            async for _ in cur:
+                pass
+
+    tasks = [fn() for _ in range(concurrency)]
+    await asyncio.gather(*tasks)
+
+
+async def cursor_motor(
+    collection: MotorCollection,
+    iterations: int,
+    concurrency: int,
+):
+    async def fn():
+        for _ in range(iterations):
+            cur = collection.aggregate(pipeline=[{'$match': {'a': {'$gt': 10}}}])
+            async for _ in cur:
+                pass
+
+    tasks = [fn() for _ in range(concurrency)]
+    await asyncio.gather(*tasks)
+
+
+@pytest.mark.find
+@pytest.mark.parametrize('concurrency', CONCURENCIES)
+def test_find_one_mongojet(aio_benchmark, mongojet_read_collection, concurrency):
+    aio_benchmark(find, mongojet_read_collection, ITERATIONS, concurrency)
+
+
+@pytest.mark.find
+@pytest.mark.parametrize('concurrency', CONCURENCIES)
+def test_find_one_motor(aio_benchmark, motor_read_collection, concurrency):
+    aio_benchmark(find, motor_read_collection, ITERATIONS, concurrency)
+
+
+@pytest.mark.find
+@pytest.mark.parametrize('concurrency', CONCURENCIES)
+def test_find_one_pymongo(aio_benchmark, pymongo_read_collection, concurrency):
+    aio_benchmark(find, pymongo_read_collection, ITERATIONS, concurrency)
+
+
+@pytest.mark.cursor
+@pytest.mark.parametrize('concurrency', CONCURENCIES)
+def test_cursor_mongojet(aio_benchmark, mongojet_read_collection, concurrency):
+    aio_benchmark(cursor_mongojet, mongojet_read_collection, ITERATIONS, concurrency)
+
+
+@pytest.mark.cursor
+@pytest.mark.parametrize('concurrency', CONCURENCIES)
+def test_cursor_motor(aio_benchmark, motor_read_collection, concurrency):
+    aio_benchmark(cursor_motor, motor_read_collection, ITERATIONS, concurrency)
+
+
+@pytest.mark.insert
+@pytest.mark.parametrize('concurrency', CONCURENCIES)
+def test_insert_mongojet(aio_benchmark, mongojet_update_collection, concurrency):
+    aio_benchmark(insert, mongojet_update_collection, ITERATIONS, concurrency)
+
+
+@pytest.mark.insert
+@pytest.mark.parametrize('concurrency', CONCURENCIES)
+def test_insert_motor(aio_benchmark, motor_update_collection, concurrency):
+    aio_benchmark(insert, motor_update_collection, ITERATIONS, concurrency)
+
+
+@pytest.mark.insert
+@pytest.mark.parametrize('concurrency', CONCURENCIES)
+def test_insert_pymongo(aio_benchmark, pymongo_update_collection, concurrency):
+    aio_benchmark(insert, pymongo_update_collection, ITERATIONS, concurrency)
+
+
+# python -m pytest ./benchmark -m find -v -s
